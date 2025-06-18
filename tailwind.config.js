f2056feabeb0a63d32e7a8463f6490770a53d091
erredev-JS <rodrigozapata968@gmail.com>
2025-06-18 20:00:54 -0300

    Also 0.1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['lubuntu', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
